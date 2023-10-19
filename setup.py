b351fbdc9550b6b05984ffdef47e31186adca8dc
Emmanuel Ajuelos <60262354+ajuelosemmanuel@users.noreply.github.com>
2023-10-19 18:15:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     author_email="ajuelosemmanuel@gmail.com",
     install_requires=["iso-639","pycountry","httpx","selenium", "gpxpy", "folium", "asyncio", "pandas"],
     description="philINT is a library that allows OSINT investigators to retrieve and organize data from an email address or an username.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     include_package_data=True,
     url="https://github.com/ajuelosemmanuel/philINT",
     entry_points = {
