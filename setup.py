68bd32fd89244c4a7d6600c3f7ddac1bb7e0cd6a
Emmanuel Ajuelos <60262354+ajuelosemmanuel@users.noreply.github.com>
2023-10-19 18:44:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ except(IOError, ImportError):
 
 setup(
     name="philINT",
-    version="1.00",
+    version="0.1.a",
     packages=find_packages(),
     author="Emmanuel Ajuelos",
     author_email="ajuelosemmanuel@gmail.com",
     install_requires=["iso-639","pycountry","httpx","selenium", "gpxpy", "folium", "asyncio", "pandas"],
-    description="philINT is a library that allows OSINT investigators to retrieve and organize data from an email address or an username.",
     long_description_content_type='text/markdown',
     long_description=long_description,
+    description="philINT is a library that allows OSINT investigators to retrieve and organize data from an email address or an username.",
     include_package_data=True,
     url="https://github.com/ajuelosemmanuel/philINT",
     entry_points = {
